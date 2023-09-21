96c83b5418829ed8a6d7a4943738cb4493acf410
NataliaMarconBotteon <nataliamarconbotteon@gmail.com>
2023-09-21 20:58:16 -0300

    lista de financiamento

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
+import modelo.Financiamento;
+
+import java.util.ArrayList;
+
 public class Main {
+
     public static void main(String[] args) {
+        ArrayList<Financiamento> listaFinanciamentos = new ArrayList<Financiamento>();
         util.InterfaceUsuario interfaceUsuario = new util.InterfaceUsuario();
+        int i;
 
-        double valorImovel = interfaceUsuario.pedirValorImovel();
-        int prazoFinanciamento = interfaceUsuario.pedirPrazoFinanciamento();
-        double taxaJuros = interfaceUsuario.pedirTaxaJuros();
-
-        modelo.Financiamento financiamento = new modelo.Financiamento(valorImovel, prazoFinanciamento, taxaJuros);
-
-        financiamento.imprimir();
+        for (i=1; i<=4; i++){
+            System.out.println("Financiamento " + i);
+            double valorImovel = interfaceUsuario.pedirValorImovel();
+            int prazoFinanciamento = interfaceUsuario.pedirPrazoFinanciamento();
+            double taxaJuros = interfaceUsuario.pedirTaxaJuros();
+            modelo.Financiamento financiamento = new modelo.Financiamento(valorImovel, prazoFinanciamento, taxaJuros);
+            listaFinanciamentos.add(financiamento);
+        }
+        for (Financiamento item : listaFinanciamentos){
+            item.imprimir();
+        }
     }
 }
